5fd7624196c52e0bcf05e26a3d0a1008d228cbb9
MartinDyke <mdyke91@gmail.com>
2018-09-03 11:17:14 +0100

    Updated

--- KML File Editor.py
+++ KML File Editor.py
@@ -4,33 +4,35 @@
 import os
 
 if __name__ == "__main__":
-
-    chk = os.listdir()
-
-    inp = input("Please enter file name:")
-
-    if inp[-4:] != ".kml":  inp += ".kml"
-
-    while inp not in chk:   # Loop until file is found in current directory. kml extension not required
-        print("Error: File not found in current directory.")
-        inp = input("Please enter file name: ")
-        if inp[-4:] != ".kml":
-            inp += ".kml"
-
-    out = input("Enter name of output file: ")
-
-    if out[-4:] != ".kml":
-        out += ".kml"
-    track = input("Please enter name of track: ")
-
-    f = "".join(open(inp,"r").readlines())
-
-    f = f[:f.find("<name>")+6] + track + f[f.find("</name>"):]
-
-    while f.find("<Point>") != -1:
-        f=f[:f.find("<Point>")]+f[f.find("</Point>")+8:]
-
-    while f.find("<IconStyle>") != -1:
-        f = f[:f.find("<IconStyle>")] + f[f.find("</IconStyle>")+12:]
-
-    open(out,"w").write(f)
+	
+	chk = os.listdir()
+	
+	# Enter the input file. Kml extension is not required
+	inp = input("Please enter file name:")
+	if inp[-4:] != ".kml":  inp += ".kml"
+	while inp not in chk:
+		print("Error: File not found in current directory.")
+		inp = input("Please enter file name: ")
+		if inp[-4:] != ".kml":
+			inp += ".kml"
+	
+	# Set output file, kml extension not required, will be added if not inputted.
+	out = input("Enter name of output file: ")
+	if out[-4:] != ".kml":
+		out += ".kml"
+	
+	track = input("Please enter name of track: ")  # Desired name of the track
+	
+	f = "".join(open(inp, "r").readlines())
+	
+	f = f[:f.find("<name>") + 6] + track + f[f.find("</name>"):]
+	
+	# Strips the data stored within <Point>
+	while f.find("<Point>") != -1:
+		f = f[:f.find("<Point>")] + f[f.find("</Point>") + 8:]
+	
+	# Strips the data stored within <IconStyle>
+	while f.find("<IconStyle>") != -1:
+		f = f[:f.find("<IconStyle>")] + f[f.find("</IconStyle>") + 12:]
+	
+	open(out, "w").write(f)
